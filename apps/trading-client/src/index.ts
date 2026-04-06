ae4f129cfaf724d6c727cc1ded4120902f404238
PuguForex <125800762+PuguForex@users.noreply.github.com>
2026-04-06 14:02:23 +0000

    Test CI failure

--- apps/trading-client/src/index.ts
+++ apps/trading-client/src/index.ts
@@ -2,6 +2,8 @@ import { loadTrades } from "./services/TradeService";
 import { filterTradesBySymbol, processTrades, calculateSummary } from "./services/TradeProcessor";
 import { printSummary } from "./utils/ReportPrinter";
 
+const x: string = 123; // type error
+
 async function main(): Promise<void> {
   try {
     const symbol = process.argv[2]; // now first arg is symbol
